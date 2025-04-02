b7d911d49292cf4c045f35d12e3bc7c0d913e116
AkashShetty2001 <akashkd9911@gmail.com>
2025-04-02 12:33:16 +0530

    practice Questions on conditional statements

---
+++ src/basics/conditional_Statements/practiceQuestions/LargestOf2.java
@@ -0,0 +1,15 @@
+package basics.conditional_Statements.practiceQuestions;
+
+public class LargestOf2 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b=50;
+
+        //conditional statements
+        if(a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
