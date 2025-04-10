792fc6a1c41cf9061acb1cfda536f3e565b8354f
AkashShetty2001 <akashkd9911@gmail.com>
2025-04-10 12:31:07 +0530

    printing square pattern

---
+++ src/basics/loops/practiceQuestion/SquarePattern.java
@@ -0,0 +1,26 @@
+package basics.loops.practiceQuestion;
+
+/*
+    ****
+    ****
+    ****
+    ****
+ */
+public class SquarePattern {
+    public static void main(String args[]){
+
+        // using for
+        for(int i=1;i<=4;i++){
+            System.out.println("****");
+        }
+
+        System.out.println();
+
+        //using while
+        int i=1;
+        while(i<=4){
+            System.out.println("****");
+            i++;
+        }
+    }
+}
